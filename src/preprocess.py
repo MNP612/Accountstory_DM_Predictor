a92f9c4e81a00d1c44627c660f1e2ce700b1c62b
Marvin Pohl <marvin.nicolas@me.com>
2022-01-10 22:28:05 -0800

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,4 @@
-import numpy as np
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import datetime
-import re
-import os
 
-my_path = os.path.abspath(os.path.dirname(__file__))
 
 def process_data(data):
 
