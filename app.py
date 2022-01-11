d7235fcf9d6b84305968e5b3981c7de3148c3b90
Marvin Pohl <marvin.nicolas@me.com>
2022-01-10 21:45:24 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
-import preprocess
+#import preprocess
 import os
 import tensorflow as tf
 from flask import Flask, request, render_template
 
 my_path = os.path.abspath(os.path.dirname(__file__))
 
-data = pd.read_json(my_path + '/combined_dataset_20210930.json')#.loc[[0]].reset_index(drop=True)
-data = preprocess.process_data(data)
+#data = pd.read_json(my_path + '/combined_dataset_20210930.json')#.loc[[0]].reset_index(drop=True)
+#data = preprocess.process_data(data)
 
 model = tf.keras.models.load_model(my_path + '/model')
 
@@ -26,7 +26,8 @@ def home():
        
     if request.method == 'POST':
         employee_index = request.form.get("name_employee")
-        pred_data = np.array(data)[int(employee_index)][:314].astype(float).reshape(1,-1)
+        #pred_data = np.array(data)[int(employee_index)][:314].astype(float).reshape(1,-1)
+        pred_data = np.zeros((1,314))
         res = pred(pred_data)
         res = round(res[0][0], 2)
         return render_template("index.html",
